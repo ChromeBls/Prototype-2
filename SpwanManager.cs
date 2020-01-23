7da0b7677d18e5b77c4233bbf27ad476f2ffe79d
ChromeBls <56165722+ChromeBls@users.noreply.github.com>
2020-01-23 07:27:33 -0800

    Add files via upload

---
+++ SpwanManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwanManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(animalIndex, animalIndex, 18), animalPrefabs[0].transform.rotation);
+        }
+
+
+    }
+}
